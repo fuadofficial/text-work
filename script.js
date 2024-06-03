e1535cecccd74d1491cd69ec56a9ca04b48ed36d
fuad <ahmadufuadoorakam@gmail.com>
2024-06-03 06:05:47 +0530

    created 6 th case work

--- script.js
+++ script.js
@@ -14,25 +14,60 @@
 
 // work 5
 
-document.getElementById("submit").addEventListener("click", () => {
-    const mark = parseInt(document.getElementById("input").value);
-    let gradeElement = document.getElementById('grade');
-    let grade = '';
+// document.getElementById("submit").addEventListener("click", () => {
+//     const mark = parseInt(document.getElementById("input").value);
+//     let gradeElement = document.getElementById('grade');
+//     let grade = '';
 
-    if (mark > 100 || mark < 0) {
-        grade = 'Invalid Mark. Enter your mark between 0 to 100';
-    } else if (mark >= 90) {
-        grade = 'You have "A" grade';
-    } else if (mark >= 80) {
-        grade = 'You have "B" grade';
-    } else if (mark >= 70) {
-        grade = 'You have "C" grade';
-    } else if (mark >= 60) {
-        grade = 'You have "D" grade';
-    } else if (mark >= 50) {
-        grade = 'You have "E" grade';
-    } else {
-        grade = 'Yor are faild';
+//     if (mark > 100 || mark < 0) {
+//         grade = 'Invalid Mark. Enter your mark between 0 to 100';
+//     } else if (mark >= 90) {
+//         grade = 'You have "A" grade';
+//     } else if (mark >= 80) {
+//         grade = 'You have "B" grade';
+//     } else if (mark >= 70) {
+//         grade = 'You have "C" grade';
+//     } else if (mark >= 60) {
+//         grade = 'You have "D" grade';
+//     } else if (mark >= 50) {
+//         grade = 'You have "E" grade';
+//     } else {
+//         grade = 'Yor are faild';
+//     }
+//     gradeElement.innerHTML = grade;
+// });
+
+// <!-- work 6-->
+
+document.getElementById("submit").addEventListener("click", () => {
+    const dayNumber = parseInt(document.getElementById("input").value);
+    let day;
+
+    switch (dayNumber) {
+        case 1:
+            day = "Sunday";
+            break;
+        case 2:
+            day = "Monday";
+            break;
+        case 3:
+            day = "Tuesday";
+            break;
+        case 4:
+            day = "Wednesday";
+            break;
+        case 5:
+            day = "Thursday";
+            break;
+        case 6:
+            day = "Friday";
+            break;
+        case 7:
+            day = "Saturday";
+            break;
+        default:
+            day = "Invalid Input. Please enter a number between 1 and 7.";
     }
-    gradeElement.innerHTML = grade;
+
+    document.getElementById("day").innerHTML = day;
 });
